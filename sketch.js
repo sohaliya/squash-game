b782c62ccc3257809e6e1be23a5e76e231a636fc
sohaliya <69452384+sohaliya@users.noreply.github.com>
2020-08-10 15:58:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var ball,ballimg,paddleimg,paddle;
+
+function preload() {
+  ballimg=loadImage("ball.png");
+  paddleimg=loadImage("paddle.png");
+}
+function setup() {
+  createCanvas(400, 400);
+   
+  ball=createSprite(60,200,20,20);
+  ball.addImage(ballimg);
+  ball.velocityX=9;
+  
+  paddle=createSprite(200,200,20,100);
+  paddle.addImage(paddleimg);
+
+}
+
+function draw() {
+  background(205,153,0);
+  edges=createEdgeSprites();
+  
+  ball.bounceOff(edges[0]);
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+  ball.bounceOff(paddle,explosion);
+  paddle.collide(edges);
+                 
+  
+  
+  
+
+  
+  /* Prevent the paddle from going out of the edges */ 
+  
+  
+  if(keyDown(UP_ARROW))
+  {
+     paddle.y=paddle.y-20;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+  paddle.y=paddle.y+20;
+  }
+ 
+  drawSprites();
+  
+}
+
+function explosion()
+{
+  ball.velocityY=random(-8,8);
+}
+
